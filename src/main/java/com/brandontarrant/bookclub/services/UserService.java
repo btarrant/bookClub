39583c99b69683f431816910261f345a820254fe
Brandon Tarrant <brandontarrant04@gmail.com>
2022-06-14 12:26:36 -0400

    first commit

---
+++ src/main/java/com/brandontarrant/bookclub/services/UserService.java
@@ -0,0 +1,97 @@
+package com.brandontarrant.bookclub.services;
+
+import java.util.Optional;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+
+import com.brandontarrant.bookclub.models.LoginUser;
+import com.brandontarrant.bookclub.models.User;
+import com.brandontarrant.bookclub.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+    @Autowired
+    private UserRepository userRepo;
+    
+    
+    public User register(User newUser, BindingResult result) {
+        
+        
+        // Reject if email is taken
+    	Optional<User> userLookUp = userRepo.findByEmail(newUser.getEmail());
+    	if (userLookUp.isPresent()) {
+    		result.rejectValue("email", "Unique", "Account with this email already exists.");
+    	}
+    	
+        // Reject if password doesn't match confirmation
+    	if(!newUser.getPassword().equals(newUser.getConfirm())) {
+    	    result.rejectValue("confirm", "Matches", "The Confirm Password must match Password!");
+    	}
+        
+        // Return null if result has errors
+    	if(result.hasErrors()) {
+    		return null;
+    	}
+    
+        // Hash and set password, save user to database
+    	String hashed = BCrypt.hashpw(newUser.getPassword(), BCrypt.gensalt());
+    	newUser.setPassword(hashed);
+    	
+    	newUser = repo.save(newUser);
+    	System.out.println("New user created with ID: " + newUser.getId());
+    	
+        return newUser;
+    }
+    
+    public User login(LoginUser newLogin, BindingResult result) {
+    	// Find user in the DB by email
+        // Reject if NOT present
+    	Optional<User> userLookUp = userRepo.findByEmail(newLogin.getEmail());
+    	if (!userLookUp.isPresent()) {
+    		result.rejectValue("email", "MissingAccount", "No account found.");
+    		return null;
+    	}
+    	// User exists, retrieve user from DB
+    	User user = userLookUp.get();
+        
+        // Reject if BCrypt password match fails
+    	if(!BCrypt.checkpw(newLogin.getPassword(), user.getPassword())) {
+    	    result.rejectValue("password", "Matches", "Invalid Password!");
+    	}
+    	
+        // Return null if result has errors
+    	if(result.hasErrors()) {
+    		return null;
+    	}
+    	
+        // Otherwise, return the user object
+        return user;
+    }
+
+	public User findByEmail(String email) {
+		
+		Optional<User> result = repo.findByEmail(email);
+		if(result.isPresent()) {
+			return result.get();
+		}
+		
+		return null;
+	}
+	
+	public User findById(Long id) {
+		
+		Optional<User> result = repo.findById(id);
+		if(result.isPresent()) {
+			return result.get();
+		}
+		
+		return null;
+	}
+}
